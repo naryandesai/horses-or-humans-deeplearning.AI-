ff63720b0c1763378f31f2d64fd4f52d8e5dcb2e
naryandesai <narayandesai@utexas.edu>
2021-06-13 14:54:25 -0400

    Add files via upload

---
+++ horsesorhumans.py
@@ -0,0 +1,133 @@
+import os
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from os import getcwd
+
+path_inception = f"{getcwd()}/../tmp2/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
+
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+local_weights_file = path_inception
+
+pre_trained_model =  InceptionV3(input_shape = (150, 150, 3),
+                                include_top = False,
+                                weights = None)
+
+pre_trained_model.load_weights(local_weights_file)
+
+for layer in pre_trained_model.layers:
+ layer.trainable = False
+
+pre_trained_model.summary()
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.97):
+      print("\nSufficient training level reached.")
+      self.model.stop_training = True
+
+from tensorflow.keras.optimizers import RMSprop
+
+
+x = layers.Flatten()(last_output)
+x = layers.Dense(1024, activation='sigmoid')(x)
+x = layers.Dropout(0.2)(x)
+x = layers.Dense  (1, activation = 'sigmoid')(x)
+
+model = Model( pre_trained_model.input, x)
+
+model.compile(optimizer = RMSprop(lr=0.0001),
+              loss = 'binary_crossentropy',
+              metrics = ['acc'])
+
+model.summary()
+
+path_horse_or_human = f"{getcwd()}/../tmp2/horse-or-human.zip"
+
+path_validation_horse_or_human = f"{getcwd()}/../tmp2/validation-horse-or-human.zip"
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import zipfile
+import shutil
+
+shutil.rmtree('/tmp')
+local_zip = path_horse_or_human
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/training')
+zip_ref.close()
+
+local_zip = path_validation_horse_or_human
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation')
+zip_ref.close()
+
+train_dir = '/tmp/training'
+validation_dir = '/tmp/validation'
+
+train_horses_dir = os.path.join(train_dir, 'horses')
+train_humans_dir = os.path.join(train_dir, 'humans')
+validation_horses_dir = os.path.join(validation_dir, 'horses')
+validation_humans_dir = os.path.join(validation_dir, 'humans')
+
+train_horses_fnames = os.listdir(train_horses_dir)
+train_humans_fnames = os.listdir(train_humans_dir)
+validation_horses_fnames = os.listdir(validation_horses_dir)
+validation_humans_fnames = os.listdir(validation_humans_dir)
+
+print(len(train_horses_fnames))
+print(len(train_humans_fnames))
+print(len(validation_horses_fnames))
+print(len(validation_humans_fnames))
+
+train_datagen = ImageDataGenerator(rescale = 1./255.,
+                                   rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator( rescale = 1./255.)
+
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size = 20,
+                                                    class_mode = 'binary',
+                                                    target_size = (150, 150))
+
+validation_generator =  test_datagen.flow_from_directory(validation_dir,
+                                                          batch_size  = 20,
+                                                          class_mode  = 'binary',
+                                                          target_size = (150, 150))
+
+history = model.fit_generator(train_generator,
+            validation_data = validation_generator,
+            steps_per_epoch = 50,
+            epochs = 3,
+            validation_steps = 50,
+            verbose = 1,
+            callbacks = [myCallback()],)
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend(loc=0)
+plt.figure()
+
+
+plt.show()
